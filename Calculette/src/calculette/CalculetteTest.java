e7d193009e73654d408f34c1485ec4dfe91a2981
ELBedoui Mouoifek <bedoui.mouoifek@gmail.com>
2013-11-27 21:07:49 +0100

    ceci est un test dernier

---
+++ Calculette/src/calculette/CalculetteTest.java
@@ -0,0 +1,14 @@
+package calculette;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculetteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Calculette/src/calculette/prima.java
@@ -0,0 +1,11 @@
+package calculette;
+
+public class prima {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Soustraction sc=new Soustraction();
+System.out.print( sc.calcul(8, 9));;
+	}
+
+}
